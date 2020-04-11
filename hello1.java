15fdd3c5c6bc641d74646d70552a168dc5f70db6
Wincy <wincythomas1@gmail.com>
2020-04-11 17:53:15 +0530

    fruit orange added

--- hello1.java
+++ hello1.java
@@ -4,6 +4,7 @@ public class hello {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 	System.out.println("apple");
+        System.out.println("orange");
 
     }
 
