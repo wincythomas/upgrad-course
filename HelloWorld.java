5fc457e468e952bfe7d1cc3137bd008dbba34cde
Wincy <wincythomas1@gmail.com>
2020-04-11 11:10:05 +0530

    new file hello

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-// Your First Program to print hello world 
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-	System.out.println("Good, Morning!");
-	System.out.println("Good, Morning, Wincy!");
-    }
-}
