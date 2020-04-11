6cf03996b9be93122d6e045a19f41f534d31f09f
Wincy <wincythomas1@gmail.com>
2020-04-11 09:54:24 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Your First Program to print hello world 
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+	System.out.println("Good, Morning!"); 
+    }
+}
