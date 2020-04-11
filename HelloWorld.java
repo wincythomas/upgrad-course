d734d5c03b106963a7c9465cc0a5c532ea72276a
Wincy <wincythomas1@gmail.com>
2020-04-11 10:36:36 +0530

    added name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-	System.out.println("Good, Morning!"); 
+	System.out.println("Good, Morning!");
+	System.out.println("Good, Morning, Wincy!");
     }
 }
